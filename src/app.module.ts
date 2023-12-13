9c8971e51b0c5613ecdf2e8173110fb2ed5edc09
sungh0lim <sungho.lim1@wesang.com>
2023-12-14 00:17:41 +0900

    feat: postgresql 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.PG_DB_USERNAME,
+      password: process.env.PG_DB_PASSWORD,
+      database: 'sampleapi_db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
